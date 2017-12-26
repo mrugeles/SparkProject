d675054be7883df6b26655b3364c775c7fb78bbf
Mario Rugeles <Mario_Rugeles@comcast.com>
2017-12-26 14:46:18 -0500

    Reading from resources

--- src/main/scala/BatchFileReader.scala
+++ src/main/scala/BatchFileReader.scala
@@ -7,12 +7,15 @@ import org.apache.spark.SparkConf
 object BatchFileReader {
   def main(args: Array[String]) {
     import org.apache.spark.SparkConf
-    val conf = new SparkConf().setAppName("Batch File Reader").setMaster("local[2]").set("spark.executor.memory", "1g")
+    val conf = new SparkConf()
+      .setAppName("Batch File Reader")
+      .setMaster("local[2]")
+      .set("spark.executor.memory", "1g")
     val sc = new SparkContext(conf)
-    val textFile = sc.textFile("file:///users.csv")
+    val textFile = sc.textFile(getClass.getResource("/users.csv").getPath)
 //    val textfileMap = textFile.
 
-    val genderMap = textFile.flatMap(line=>line.split(","))
+    //val genderMap = textFile.flatMap(line=>line.split(","))
 
 
 //    val countPrep = tokenizedFileData.map(word=>(word, 1))
